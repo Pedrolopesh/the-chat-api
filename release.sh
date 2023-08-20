74885156f841fb728fd5cd580d170f0da6d86800
Pedro Lopes <pedro.lopes@acct.global>
2023-08-20 20:05:09 -0300

    release new version 0.1.0

--- release.sh
+++ release.sh
@@ -5,16 +5,17 @@ git checkout develop
 git pull origin develop
 
 # Criar uma branch de release a partir da develop
-release_branch="release/$(date +'%Y%m%d_%H%M%S')"
+release_version="0.1.0"
+release_branch="release/$release_version"
 git checkout -b "$release_branch"
 
 # Faça as alterações necessárias para a versão de release (atualizações, correções, etc.)
 
 # Commit das alterações na branch de release
-git commit -am "Preparando para a release"
+git commit -am "release new version $release_version"
 
 # Criar uma tag com a versão de release
-tag="v$(date +'%Y.%m.%d')"
+tag="v$release_version-beta"
 git tag "$tag"
 
 # Empurrar a branch de release e a tag para o repositório remoto
